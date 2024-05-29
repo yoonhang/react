09682be55d43dd418e0251d524f20e76c2dcccf9
yoonhang <com717942@gmail.com>
2024-05-29 12:56:15 +0900

    comment

--- src/components/Customer.js
+++ src/components/Customer.js
@@ -4,6 +4,28 @@ class Customer extends React.Component {
     render() {
         return (
             <div>
+                <CustomerProfile id={this.props.id} img={this.props.img}/>
+                <CustomerInfo name={this.props.name} birthday={this.props.birthday} gender={this.props.gender} job={this.props.job}/>
+            </div>
+        )
+    }
+}
+
+class CustomerProfile extends React.Component {
+    render() {
+        return (
+            <div>
+                <p>{this.props.id}</p>
+                <img src={this.props.img} alt="img"></img>
+            </div>
+        )
+    }
+}
+
+class CustomerInfo extends React.Component {
+    render() {
+        return (
+            <div>
                 <h2>{this.props.name}</h2>
                 <p>{this.props.birthday}</p>
                 <p>{this.props.gender}</p>
@@ -12,6 +34,5 @@ class Customer extends React.Component {
         )
     }
 }
-
 
 export default Customer;
